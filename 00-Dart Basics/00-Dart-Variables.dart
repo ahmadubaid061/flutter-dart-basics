1fe85285822165b2edec53b2fa4cc108b7de39a6
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-08-11 11:38:03 +0500

    variables and datatypes in Dart

--- 00-Dart Basics/00-Dart-Variables.dart
+++ 00-Dart Basics/00-Dart-Variables.dart
@@ -8,10 +8,13 @@ void main(){
   // ---------------------double data type
   double gpa=3.55;
   print("my cgpa is $gpa");
-  // ---------------------var data type
+  // ---------------------var data type (it has a global scope)
   var phone=03139215242;
   print("my phone is $phone");
-  // ---------------------dynamic data type
+  // ---------------------dynamic data type (its content can be changes)
   dynamic currentCity='Islamabad';
   print("my current City is $currentCity");
+  // ---------------------const data type  (its content cannot be changed)
+  const rollNo=11;
+  print("my roll No is $rollNo");
 }
