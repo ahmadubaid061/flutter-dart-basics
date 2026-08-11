d4eaf9e79031e5d34aec12298433e0b6e9804b10
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-08-11 11:15:41 +0500

    variables in Dart

---
+++ Dart Basics/00-Dart-Variables.dart
@@ -0,0 +1,17 @@
+void main(){
+  // ---------------------string data type
+  String name="Ubaid";
+  print("my name is $name");
+  // ---------------------integer data type
+  int age=24;
+  print("my age is  $age");
+  // ---------------------double data type
+  double gpa=3.55;
+  print("my cgpa is $gpa");
+  // ---------------------var data type
+  var phone=03139215242;
+  print("my phone is $phone");
+  // ---------------------dynamic data type
+  dynamic currentCity='Islamabad';
+  print("my current City is $currentCity");
+}
